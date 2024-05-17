beb49e5436cb05229ea0f24fcc43fd8e05740bf6
Elvis <elvisptavares@gmail.com>
2024-05-17 10:17:32 -0300

    ...

---
+++ src/app/repositories/PostRepository.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PostModel = require("../models/Post");
+
+class PostRepository {
+    findAll() {
+        
+    }
+}
+
+export default new PostRepository()
